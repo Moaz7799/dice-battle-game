bed5858eb9470cc50dd4a2c83550b695e083279d
Moaz7799 <moazbmc@gmail.com>
2024-05-17 08:17:08 +0300

    Added Dice Battle Game with HTML, CSS, and JavaScript files.

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# dice-battle-game
-Dice Battle Game: Roll the dice, hold your score, and be the first to reach 100 points!
+# Dice Battle Game
+
+Simple Dice Battle Game: Roll the dice, hold your score, and be the first to reach 100 points!
+
+## How to Play
+
+**Dice Battle Game Rules:**
+
+1. **Objective:** Be the first player to reach 100 points.
+
+2. **Gameplay:**
+
+   - The game has 2 players, playing in rounds.
+   - In each turn, a player rolls a dice as many times as they wish. Each result gets added to their round score.
+   - **BUT**, if the player rolls a 1, their round score gets lost. After that, it's the next player's turn.
+   - The player can choose to 'Hold', which means that their round score gets added to their total score. After that, it's the next player's turn.
+
+3. **Winning the Game:**
+   - The first player to reach 100 points on the global score wins the game.
+
+**Controls:**
+
+- **Roll Dice:** Click the "🎲 Roll dice" button to roll the dice.
+- **Hold:** Click the "📥 Hold" button to add your current score to the global score and pass the turn to the next player.
+- **New Game:** Click the "🔄 New game" button to reset the game.
+
+Enjoy playing!
---
+++ script.js
@@ -0,0 +1,95 @@
+'use strict';
+
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const current0El = document.getElementById('current--0');
+const score1El = document.getElementById('score--1');
+const current1El = document.getElementById('current--1');
+const diceEl = document.querySelector('.dice');
+
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+//starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+let scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+let playing = true;
+
+const switchPlayer = () => {
+  currentScore = 0;
+  document.getElementById(`current--${activePlayer}`).textContent =
+    currentScore;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
+//Rolling dice Functionality
+btnRoll.addEventListener('click', function () {
+  if (playing) {
+    const dice = Math.ceil(Math.random() * 6);
+
+    diceEl.src = `dice-${dice}.png`;
+    diceEl.classList.remove('hidden');
+
+    if (dice !== 1) {
+      // Add dive to current score
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // Switch to next player
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    scores[activePlayer] += currentScore;
+
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    if (scores[activePlayer] >= 100) {
+      playing = false;
+      diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+btnNew.addEventListener('click', function () {
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  diceEl.classList.add('hidden');
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.remove('player--winner');
+  document
+    .querySelector(`.player--${activePlayer === 0 ? 1 : 0}`)
+    .classList.remove('player--active');
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.add('player--active');
+  playing = true;
+});
